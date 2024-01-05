860548892000b0803790f3fb16d1b5590f5337a2
abhijeetkumarshukla <shuklaabhijeet510@gmail.com>
2024-01-05 14:31:55 +0530

    day2 added

---
+++ day2.js
@@ -0,0 +1,12 @@
+// problem 1
+console.log('Masai School');
+console.log("A Transformation in Education")
+
+
+// problem 2
+console.log(number*50)
+
+
+// problem 3
+
+console.log(one*two*three*four*five*six)
