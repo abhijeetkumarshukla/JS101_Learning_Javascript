5d409f2f5f1557dece68078c891b3fff7618f93c
abhijeetkumarshukla <shuklaabhijeet510@gmail.com>
2024-01-05 11:17:44 +0530

    added

---
+++ day1.js
@@ -0,0 +1,45 @@
+// Problem 1
+//Print "Masai School" in the console followed by "A Transformation in Education" in next line
+
+console.log("Masai School")
+console.log("A Transformation in Education")
+
+// Problem 2
+// Define a variable called name with your Name as the assigned value
+// Print the value stored in the variable name
+// Change the variable to store your father's name
+// Print the value stored in the variable name
+// Change the variable again to store your mother's name.
+// Print the value stored in the variable name
+
+var MyName = "Abhijeet Shukla"
+console.log(MyName)
+
+var fatherName = "Munindra Nath Shukla"
+console.log(fatherName)
+
+var motherName ="Munni Shukla"
+console.log(motherName)
+
+
+
+// Problem 3
+// Create a variable name to store your name
+// Create a variable age to store your age
+// Concatenate and console them in a new string to get the output like this : ("My name is"+ name +"and my age is"+ age +".")
+
+
+var MyName= "Abhijeet Shukla"
+var age=23;
+
+console.log("MY name is " + MyName + " and my age is " + age +'.');
+
+
+// Problem 4
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+// Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration
+
+console.log("RollNo    Name       School    Grade      Section    Physics   Math  Hindi")
+
+console.log("  1     Abhijeet   High School    1st         B         78       84    67  ")
